b8fe0f07970257594caada33f7c3125c0577f0c6
Timothy Zhou <39917965+timzhou99@users.noreply.github.com>
2020-08-04 22:48:50 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { Home } from './Home';
+import { Resume } from './Resume';
+import { Projects } from './Projects';
+import { Films } from './Films';
+import { About } from './About';
+import { NoMatch } from './NoMatch';
+
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <React.Fragment>
+
+      <NavigationBar />
+
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path={process.env.PUBLIC_URL + "/"} component={Home} />
+
+            <Route path={process.env.PUBLIC_URL + "/resume"} component={Resume} />
+            <Route path={process.env.PUBLIC_URL + "/projects"} component={Projects} />
+            <Route path={process.env.PUBLIC_URL + "/films"} component={Films} />
+            <Route path={process.env.PUBLIC_URL + "/about"} component={About} />
+
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </Layout>
+    </React.Fragment>
+
   );
 }
 

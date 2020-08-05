3241cbd4ced55b8313290a6e864f742a92395182
Timothy Zhou <39917965+timzhou99@users.noreply.github.com>
2020-08-04 22:59:20 -0400

    Updates

---
+++ precache-manifest.9c724e08a437e790f564683d6d23991e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d65be0d7c91ef159af46813bf45e5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d933899a62558ea64b5",
+    "url": "/static/css/main.00c889a8.chunk.css"
+  },
+  {
+    "revision": "054600d34c934ebf705a",
+    "url": "/static/js/2.5f6005bc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5f6005bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d933899a62558ea64b5",
+    "url": "/static/js/main.96743028.chunk.js"
+  },
+  {
+    "revision": "e0ca6d224cc6f7e6205f",
+    "url": "/static/js/runtime-main.de46f35b.js"
+  },
+  {
+    "revision": "ef69cd230963b34100b23f7fdf5c47eb",
+    "url": "/static/media/cooking.ef69cd23.gif"
+  }
+]);

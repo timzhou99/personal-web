4a1add963d13da9a8d0502c7b21c36b2bbb5b067
Timothy Zhou <timothyzhoued96@gmail.com>
2025-07-28 23:29:12 -0400

    Added CapitalOne.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -99,6 +99,25 @@ export default class Resume extends Component {
 
                 <body className="bg-light">
 
+                <Container id="" className="bg-light">
+                    <Row>
+                        <Col className="h-100 bg-light align-self-center">
+                            <Image src={ncino} fluid></Image>
+                        </Col>
+                        <Col sm={{ span: 8, offset: 1 }}>
+                            <h4>Capital One</h4>
+                            <h5>Senior Product Manager</h5>
+                            <h6>New York, NY &nbsp; |  &nbsp; Mar 2025 - Present</h6>
+                            <p>
+                                Lead product strategy to accelerate customer growth across Capital One’s Retail Bank, with a focus on onboarding, funding, and engagement. 
+                                Own end-to-end experiences across Web, iOS, and Android platforms, as well as the backend systems and APIs that support them. Currently leading 
+                                onboarding initiatives for customers transitioning from a newly integrated external banking platform, ensuring a seamless and scalable experience 
+                                that deepens product adoption and long-term engagement.
+                            </p>
+                        </Col>
+                    </Row>
+                </Container>
+
                 <Container id="latest-experience" className="bg-light">
                     <Row>
                         <Col className="h-100 bg-light align-self-center">
@@ -107,12 +126,12 @@ export default class Resume extends Component {
                         <Col sm={{ span: 8, offset: 1 }}>
                             <h4>American Express</h4>
                             <h5>Senior Associate Product Manager</h5>
-                            <h6>New York, NY &nbsp; |  &nbsp; Sep 2023 - Present</h6>
+                            <h6>New York, NY &nbsp; |  &nbsp; Sep 2023 - Mar 2025</h6>
                             <p>
-                                As a Senior Associate Product Manager on the International Application Experience team, I lead the decisioning orchestration for international card 
-                                applications across six global markets. I work closely with cross-functional teams to define and prioritize product features that drive the digital
-                                acquisition journey for our customers and prospects. In addition, I lead a team of 11 to facilitate iteration and innovation to provide the best user
-                                experience for our customer care professionals and prospective customers & applicants.
+                                Led product strategy and execution for digital card acquisition and orchestration experiences across Canada, Mexico, and the Latin America & 
+                                Caribbean regions. Owned a portfolio of 48+ card products across consumer and small business segments, including partner channels. 
+                                Collaborated with cross-functional teams to prioritize initiatives that improved application decisioning, optimized user journeys, 
+                                and scaled acquisition across diverse international markets.
                             </p>
 
                             <br />
@@ -120,9 +139,9 @@ export default class Resume extends Component {
                             <h5>Associate Product Manager</h5>
                             <h6>New York, NY &nbsp; |  &nbsp; Jun 2021 - Sep 2023</h6>
                             <p>
-                                As an Associate Product Manager at American Express, I help discover, solution, and prioritize product features for the digital acquisition journeys
-                                in the United Kingdom and Norway, Consumer and Small Business markets. In addition, as a product owner, I lead an agile and data-driven team of 11 to 
-                                facilitate iteration and innovation to provide the best user experience for our customers and prospects.
+                                Served as Product Owner for digital acquisition experiences across the UK, Norway, Canada, and Mexico, supporting 77+ card products and 
+                                partner channels. Defined and delivered customer-centric features to enhance acquisition funnels. Led an agile team of 11, using experimentation 
+                                and data to drive continuous improvements in conversion and user experience.
                             </p>
                         </Col>
                         
@@ -139,11 +158,9 @@ export default class Resume extends Component {
                             <h5>Product Management Intern</h5>
                             <h6>Wilmington, NC &nbsp; |  &nbsp; Oct 2020 - Dec 2020</h6>
                             <p>
-                                As a product management intern at nCino, I prioritized product features for the upcoming
-                                Fall release by defining epics and stories. In addition, I drove agile development on a platform
-                                feature responsible for handling over $3.2 trillion worth of assets across 290+ financial institutions.
-                                Furthermore, I helped spearhead the construction and development of an internal testing specification structure
-                                for the use of QA engineers when performing the yearly accessibility audit, which increased audit efficiency by 75%.
+                                Supported roadmap planning and agile delivery for a high-impact platform feature managing $3.2T+ in assets across 290+ financial institutions. 
+                                Defined epics and user stories for the Fall release, aligning closely with engineering and QA teams. Initiated the development of an internal 
+                                testing framework that improved accessibility audit efficiency by 75%, enabling higher product quality and compliance readiness.
                             </p>
                         </Col>
                     </Row>
@@ -159,12 +176,9 @@ export default class Resume extends Component {
                             <h5>Team Lead</h5>
                             <h6>New York, NY &nbsp; |  &nbsp; Jun 2020 - Aug 2020</h6>
                             <p>
-                                Amid the Coronavirus pandemic, I led a team of fellow NYU undergraduates to create
-                                an entrepreneurial venture to help solve an issue that the COVID-19 pandemic has
-                                brought onto society. As a team lead, I spear-headed and managed the concept-to-prototype of a job search
-                                platform that simplifies and personalizes the job application process for students. In addition, I
-                                conducted market research and analyzed data from over 150 survey responses that allowed the product
-                                team to develop a deeper understanding of the target customer's issues, pains, and desires.
+                                Led a cross-functional team to design and prototype a student-focused job search platform during the COVID-19 pandemic. 
+                                Spearheaded customer discovery, conducted market research with 150+ survey responses, and translated insights into MVP requirements. 
+                                Oversaw product development from ideation through UX design, using lean startup methodologies to iterate quickly and validate user pain points.
                             </p>
                         </Col>
                     </Row>
@@ -180,13 +194,10 @@ export default class Resume extends Component {
                             <h5>Project Management Intern</h5>
                             <h6>Bethesda, MD &nbsp; |  &nbsp; May 2019 - Sep 2019</h6>
                             <p>
-                                As a project management intern at FHL Health, I managed two clinical studies and the development of
-                                one internal product. For the two clinical studies, I managed a team of  programmers
-                                to develop and compile complete clinical trial submission packages for the FDA under budget.
-                                Furthermore, I coordinated and managed the development of an internal automated case report form generator
-                                that decreased generation times by 300%. I also prepared and presented 2 RFQs that provided price
-                                estimations and project phases which resulted in over $300,000 worth of contracts.
-
+                                Managed two FDA-bound clinical studies and led the development of an internal automation tool.
+                                Oversaw a team of programmers to deliver complete clinical trial submission packages on time and under budget. 
+                                Directed the creation of an automated case report form generator, reducing generation time by 300%. Additionally, 
+                                prepared and presented two RFQs that secured over $300K in new contracts by outlining pricing, scope, and phased delivery plans.
                             </p>
                         </Col>
                     </Row>
@@ -202,11 +213,10 @@ export default class Resume extends Component {
                             <h5>Software Engineering Intern</h5>
                             <h6>Rockville, MD &nbsp; |  &nbsp; Jun 2018 - Aug 2018</h6>
                             <p>
-                                As a software engineering intern at Essential Software, I developed a database management
-                                system that provides scientists with simple access to 8,000+ knowledge articles. I implemented
-                                a mySQL database with a ServiceNow instance that automates the workflow of each database submission
-                                to improve user communication and provide a straightforward process for scientists to review and
-                                publish various knowledge articles.
+                                Designed and implemented a database workflow platform to streamline access to 8,000+ research articles. 
+                                Automated submission and approval flows using a mySQL database integrated with ServiceNow, reducing manual 
+                                intervention and enabling more scalable knowledge sharing. Delivered an improved user experience for scientists 
+                                through simplified data entry and publication processes.
                             </p>
                         </Col>
                     </Row>
@@ -222,11 +232,9 @@ export default class Resume extends Component {
                             <h5>Marketing Analyst Intern</h5>
                             <h6>Shanghai, CN &nbsp; |  &nbsp; Jun 2017 - Aug 2017</h6>
                             <p>
-                                As a marketing analyst intern at Ipsos, I analyzed data from a focus group study on
-                                a client's telematic system that resulted in the change of 10 of its feature definitions.
-                                In addition, I prepared and presented an alysis report on telematic systems by drawing
-                                comparisons between competing products based on its features, unique qualities, prices,
-                                and preferences; allowing the client to optimize its offerings relative to its competitors.
+                                Conducted competitive feature analysis of telematics systems that led to client revisions across 10 product features. 
+                                Delivered insights via a strategic research report comparing competitor offerings, user preferences, and pricing—enabling 
+                                the client to better align their product roadmap with customer needs and market gaps.
                             </p>
                         </Col>
                     </Row>
@@ -242,9 +250,9 @@ export default class Resume extends Component {
                             <h5>Database Administrator Intern</h5>
                             <h6>Bethesda, MD &nbsp; |  &nbsp; Jun 2016 - Aug 2016</h6>
                             <p>
-                                As a database administrator intern at NIH, I transformed and implemented a user-friendly
-                                genome browser to facilitate efficient access to various genomic data sets, providing scientists
-                                with more features and customizations leading to a 250% increase in analysis capability and efficiency.
+                                Redesigned and implemented a genome browser to improve accessibility to critical genomic datasets. 
+                                The new interface enabled enhanced customization and usability, resulting in a 250% increase in researcher 
+                                productivity and data analysis efficiency.
                             </p>
                         </Col>
                     </Row>
